a54d3b1ffbdc07824e43d084ff502d9c2ef5d60f
nakul-krishnakumar <nakulkrishnakumar86@gmail.com>
2024-12-26 19:34:10 +0000

    Added prediction functionality and immplemented user interaction

--- fashion-mnist-model.py
+++ fashion-mnist-model.py
@@ -7,25 +7,27 @@ from tensorflow.keras.datasets.fashion_mnist import load_data
 # no cross validation set
 (train_imgs, train_labels), (test_imgs, test_labels) = load_data()
 
-"""
-    |-------------------- LABELS ---------------------|
-    | 0  ->  T-shirt/top                              |
-    | 1  ->  Trouser                                  |
-    | 2  ->  Pullover                                 |
-    | 3  ->  Dress                                    |
-    | 4  ->  Coat                                     |
-    | 5  ->  Sandal                                   |
-    | 6  ->  Shirt                                    |
-    | 7  ->  Sneaker                                  |
-    | 8  ->  Bag                                      |
-    | 9  ->  Ankle boot                               |
-    |-------------------------------------------------|
-"""
+labels = [
+    "t-shirt/top", #0
+    "Trouser",     #1
+    "Pullover",    #2
+    "Dress",       #3
+    "Coat",        #4
+    "Sandal",      #5
+    "Shirt",       #6
+    "Sneaker",     #7
+    "Bag",         #8
+    "Ankle boot"   #9
+]
 
 print(f'Shape of image training set: {train_imgs.shape}')
 print(f'Shape of label training set: {train_labels.shape}')
 print(f'Shape of image testing set: {test_imgs.shape}')
 print(f'Shape of label testing set: {test_labels.shape}')
+
+# Normalize the training & testing image set so model works better
+train_imgs = train_imgs / 255.0
+test_imgs = test_imgs / 255.0
 
 # Building the model
 model = tf.keras.models.Sequential([
@@ -34,3 +36,28 @@ model = tf.keras.models.Sequential([
     tf.keras.layers.Dense(128, activation=tf.nn.relu),
     tf.keras.layers.Dense(10, activation=tf.nn.softmax),
 ])
+
+# Configuring the model
+# Choosing the optimizer and loss function
+model.compile(
+    optimizer=tf.optimizers.Adam(),
+    loss=tf.losses.SparseCategoricalCrossentropy(),
+    metrics=['accuracy']
+)
+
+# Input the training data and mention the number of epochs
+model.fit( train_imgs, train_labels, epochs=5 )
+
+# Test the model on the testing data
+print("\n------- EVALUATION ON TEST SET ---------------\n")
+model.evaluate(test_imgs, test_labels)
+
+a = int(input("\nEnter test label index to predict: "))
+
+# Making the prediction
+predicted_label = model.predict(np.expand_dims(test_imgs[a], axis=0))
+
+ 
+index = np.argmax(predicted_label)
+print(f"Predicted Label: {labels[index]}")
+print(f"Actual Label: {labels[test_labels[a]]}")
